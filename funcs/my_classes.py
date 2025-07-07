795c4b188ee4a55523774e10538b4b2c459de3f6
James Kabera <kabejames@gamil.com>
2025-07-07 15:33:18 -0500

    Classes part

---
+++ funcs/my_classes.py
@@ -0,0 +1,28 @@
+from pyspark.sql.functions import year
+class Car:
+    def __init__(self, make: str, model: str, year: int, mileage: int, condition: str, color: str)-> None:
+        self.make = make
+        self.model = model
+        self.year = year
+        self.mileage = mileage
+        self.condition = condition
+        self
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+
+    def read_odometer(self):
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+    
+
