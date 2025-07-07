7520d928bf8ae9621e1ce5ff20583631c3ed040c
James Kabera <kabejames@gamil.com>
2025-07-07 13:36:54 -0500

    functions

---
+++ funcs/my_module.py
@@ -0,0 +1,12 @@
+def cube_number(my_number: int)-> int:
+    """
+    This function takes a number and returns the cube of that number.
+    """
+    return my_number**3
+
+
+def greet(name: str)-> str:
+    """
+    This function takes a name and returns a greeting.
+    """
+    return f"Hello, {name}!"
